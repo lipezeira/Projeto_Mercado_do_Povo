ea0ba4cafef4b28485c2d992cd69d36f6e7a3ca0
luizacaires <marialulupoca@gmail.com>
2023-05-24 20:34:00 -0300

    Update pagina.php

--- historia/pagina.php
+++ historia/pagina.php
@@ -1,21 +1,23 @@
 <!DOCTYPE html>
 <html>
 <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<title>teste</title>
-	<link rel="stylesheet" type="text/css" href="estilo.css">
+	<link rel="stylesheet" type="text/css" href="estilo2.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https: //fonts.googleapis.com/css2?family= Poltawski+Nowy & family= Secular+One & display =swap" rel="stylesheet">
-<link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Libre+Baskerville&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https: //fonts.googleapis.com/css2?family= Poltawski+Nowy & family= Secular+One & display =swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Libre+Baskerville&display=swap" rel="stylesheet">
 
 </head>
 <body>
     <header>
 
        <div class="container flex-center-between">
-        <img src="imagens/slogan.jpeg">
+        <img src="imagens/slogan.png">
 
       
         <nav>
@@ -33,7 +35,7 @@
             para que qualquer pessoa pudesse usar um site para anunciar e vender seus próprios produtos. <br>
             Foi um sucesso no meio da pandemia. O site era um dos mais vistos e mais utilizados pela população. <br>
             Para realizar o sonho de revolucionar o e-commerce na América Latina, <br>
-            os quatros colegas Felipe, Gabriela, Gustavo e Maria Luiza se uniram e colocaram as suas ideias em prática.
+            os quatros colegas Felipe, Gabrielle, Gustavo e Maria Luiza se uniram e colocaram as suas ideias em prática.
         </p>
 
         <div class="imagens">
