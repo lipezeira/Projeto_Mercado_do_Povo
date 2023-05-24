a3ea7cfe318f794f73f9e048ea4002cc74fc8db8
luizacaires <marialulupoca@gmail.com>
2023-05-24 16:49:07 -0300

    Create pagina.php

---
+++ historia/pagina.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>teste</title>
+	<link rel="stylesheet" type="text/css" href="estilo.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https: //fonts.googleapis.com/css2?family= Poltawski+Nowy & family= Secular+One & display =swap" rel="stylesheet">
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Libre+Baskerville&display=swap" rel="stylesheet">
+
+</head>
+<body>
+    <header>
+
+       <div class="container flex-center-between">
+        <img src="imagens/slogan.jpeg">
+
+      
+        <nav>
+            <ul>
+                <li><a href="ofertasdodia.php">Ofertas do dia</a></li>
+                <li><a href="index.php">Página Inicial</a></li>
+            </ul>
+        </nav>
+</div>
+    </header>
+    <h1><br>Desenvolvedores</h1>
+
+    <main class ="quemsomos">
+        <p class="dados"> <br><br>Mercado do povo foi fundado em 2020 com o proposito demontar uma loja online <br>
+            para que qualquer pessoa pudesse usar um site para anunciar e vender seus próprios produtos. <br>
+            Foi um sucesso no meio da pandemia. O site era um dos mais vistos e mais utilizados pela população. <br>
+            Para realizar o sonho de revolucionar o e-commerce na América Latina, <br>
+            os quatros colegas Felipe, Gabriela, Gustavo e Maria Luiza se uniram e colocaram as suas ideias em prática.
+        </p>
+
+        <div class="imagens">
+            <img id="leticia" src="imagens/felipe.jpeg">
+            <img id="leticia" src="imagens/gabi.jpeg">
+            <img id="leticia" src="imagens/gustavo.jpeg">
+            <img id="leticia" src="imagens/luiza.jpeg">
+        </div>
+
+
+    </main>
+
+    <footer id="rodape">
+    <h4>ENCONTRE-NOS AQUI:</h4>
+
+    <div class="redes-sociais">
+        
+        <a href="https://www.instagram.com/" title="Instragram"><img src="imagens/instagram.png"></a>
+        <a href="https://www.facebook.com/" title="Facebook"><img src="imagens/facebook.png"></a>
+
+</div>
+
+    <h5> &copy; 2023 MERCADO DO POVO - CNPJ: 23.070.688/0009-15</h5>
+
+    </footer>
+
+
+
+
+</body>
+</html>
