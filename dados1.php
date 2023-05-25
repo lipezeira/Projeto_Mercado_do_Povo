9906834d74fb719a956f0d30e171fbb5c0b7dcee
gabictlima <132312320+gabictlima@users.noreply.github.com>
2023-05-25 17:39:51 -0300

    Create dados1.php

---
+++ dados1.php
@@ -0,0 +1,279 @@
+<?php
+
+$produtos = [   
+    [        
+
+        "foto" => "imgs/geladeira.jpg",
+
+        "nome" => "Geladeira Brastemp Frost Free",
+
+        "valor" => "De R$ 3.800,00 ",
+
+        "valordesconto" => "Por apenas  R$ 3.399,99",
+
+        "descricao" => "
+            Marca: Brastemp;
+            <br>
+            Modelo: BRE80AK;
+            <br>
+            Cor: Inox;
+            <br>
+            Tipo de painel de controle: Externo;
+            <br>
+            Capacidade: 394 L;
+            <br>
+            Capacidade do freezer: 179 L;
+            <br>
+            Peso: 108 kg;
+            <br>
+            Quantidade de níveis de temperatura: 5;
+            <br>
+            Eficiência energética: A;
+            <br>
+            Tipo de degelo: Frost free;
+        ",      
+
+        "garantia" => "Garantia: 12 meses." 
+    ],
+    
+    [        
+
+        "foto" => "imgs/maquina_lavar.jpg",
+
+        "nome" => "Máquina de lavar automática Brastemp ",
+
+        "valor" => " De R$ 2.900,00",
+
+        "valordesconto" => "Por apenas R$ 2.299,99",
+
+        "descricao" => "
+            Marca: Brastemp;
+            <br>
+            Modelo: BWK16AB;
+            <br>
+            Cor: Prata;
+            <br>
+            Eficiência energética: A;
+            <br>
+            Capacidade: 16 kg;
+            <br>
+            Peso: 45 kg;
+            <br>
+            Quantidade de programas de lavagem: 12;
+            <br>
+            Níveis de água: 4;
+            <br>
+            Sistema de lavagem: Americano;
+            <br>
+            Tipos de carga de água: Manual, Automática;
+        ",
+
+        "garantia" => "Garantia: 16 meses." 
+    ],
+
+    [        
+
+        "foto" => "imgs/micro_ondas.jpg",
+
+        "nome" => "Microondas Panasonic",
+
+        "valor" => "De R$ 1.000,00",
+        
+        "valordesconto" => "Por apenas R$ 799,99",
+
+        "descricao" => "
+        Modelo: ST66NBRU;
+        <br>
+        Cor: Preto;
+        <br>
+        Potência (W): 900W;
+        <br>
+        Capacidade: 34L;
+        <br>
+        Níveis de potência: 10;
+        <br>
+        Peso (kg): 14,8;
+        <br>
+        Altura (cm): 32,5;
+        <br>
+        Largura (cm): 52,0;
+        <br>
+        Comprimento (cm): 43,2;
+        <br>
+        SKU: 220V: 4968523 | 127V: 4968560;
+        ",
+
+        "garantia" => "Garantia: 8 meses." 
+    ],
+
+    [        
+        "foto" => "imgs/televisao.jpg",
+
+        "nome" => "Smart Tv Tcl Led 32 Polegadas",
+
+        "valor" => "De R$ 2.200,00",
+
+        "valordesconto" => "Por apenas R$ 1.799,99",
+
+        "descricao" => "
+            Marca: Semp TCL;
+            <br>
+            Modelo: 32RS530;
+            <br>
+            Voltagem: 110V/220V;
+            <br>
+            Tipo de tela: LED;
+            <br>
+            Tipo de resolução: HD;
+            <br>
+            Tamanho da tela(polegadas): 32;
+            <br>
+            Resolução máxima: 1366x768;
+            <br>
+            Taxa de atualização da tela: 60 Hz;
+            <br>
+            Peso: 3.8 kg;
+            <br>
+            Acessórios incluídos: Controle remoto;
+        ",    
+
+        "garantia" => "Garantias: 24 meses." 
+    ],
+
+    [        
+        "foto" => "imgs/motocerra - Copia.jpg",
+
+        "nome" => "Motosserra a gasolina Toyama Farmer 49.2cc",
+
+        "valor" => "De R$ 900,00",
+
+        "valordesconto" => "Por apenas R$ 749,99",
+
+        "descricao" => "
+            Marca: Toyama;
+            <br>
+            Linha: Farmer;
+            <br>
+            Modelo: TCS53F-G2-18SN;
+            <br>
+            Cilindrada: 49.2 cc;
+            <br>
+            Tipos de combustível: Gasolina;
+            <br>
+            Potência: 2.81 hp
+            <br>
+            Peso: 5.32 kg
+            <br>
+            Velocidade máxima: 13500 rpm;
+            <br>
+            Capacidade do tanque: 0.55 L;
+            <br>
+            Acessórios incluídos: Kit ferramentas;
+        ",
+
+        "garantia" => "Garantia: 10 meses." 
+    ],
+
+    [      
+        "foto" => "imgs/quadro.jpg",
+
+        "nome" => "Quadro Decorativo 129x63",
+
+        "valor" => "De R$ 200,00",
+
+        "valordesconto" => "Por apenas R$ 159,99",
+
+        "descricao" => "
+            Comprimento: 130cm;
+            <br>
+            Altura: 63cm;
+            <br>
+            Peças menores: 25x40 cm;
+            <br>
+            Peças médias: 25x51 cm;
+            <br>
+            Peça central: 25x63 cm;
+            <br>
+            Espaçamento entre as peças: 1 cm a 2 cm;
+            <br>
+            Espessura do produto: 2 CM;
+            <br>
+            Tecido: sintético;
+            <br>
+            Tela: Tecido com impressão digital ecológica;
+            <br>
+            Bordas: Com extensão da própria imagem nas laterais;
+        ",     
+
+        "garantia" => "Este produto não possue garantia" 
+    ],
+
+    [        
+        "foto" => "imgs/notbook.webp",
+
+        "nome" => "Dell Inspiron 5000 I15-5566-A50B",
+
+        "valor" => "De R$ 3.900,00",
+
+        "valordesconto" => "Por apenas R$ 3.399,99",
+
+        "descricao" => "
+            Tipo: Windows;
+            <br>
+            Marca: Dell;
+            <br>
+            Modelo: I15-5566-A50B;
+            <br>
+            Polegadas: 15.6;
+            <br>
+            Sistema Operacional: Windows 10;
+            <br>
+            Processador: Intel Core i7;
+            <br>
+            Modelo: 7500U;
+            <br>
+            Núcleos: 2;
+            <br>
+            Velocidade: 2.7 GHz;
+            <br>
+            Cache: 4 MB;
+        ",
+
+        "garantia" => "18 meses." 
+    ],
+
+    [        
+        "foto" => "imgs/maquina_cabelo.jpg",
+
+        "nome" => "Kit Aparador De Cabelo E Pelos",
+
+        "valor" => "De R$190,00",
+
+        "valordesconto" => "Por apenas R$ 159,99",
+
+        "descricao" => "
+            Marca: Wahl;
+            <br>
+            Departamento BS: Cabelos;
+            <br>
+            Indicado para: Dia a Dia;
+            <br>
+            Fonte de Energia: 110;
+            <br>
+            Formato: Com Fio;
+            <br>
+            Classificação: Caseira;
+            <br>
+            Peso do Produto: 0,47 kg;
+            <br>
+            Dimensões Aproximadas: 5 x 19 x 5 cm;
+            <br>
+            SAC: 0800 772 2560;
+            <br>
+            À prova de água: Não;
+        ",        
+
+        "garantia" => "22 meses." 
+    ],
+
+];
