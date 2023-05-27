02184fcee56d594df17841a5f9f1c9aec31488c1
Gustavo <129855767+GUUH-programmer@users.noreply.github.com>
2023-05-26 21:06:55 -0300

    Update dados.php

--- dados.php
+++ dados.php
@@ -9,22 +9,7 @@ $produtos = [
 
         "valor" => "De R$ 3.800,00 ",
 
-        "valordesconto" => "Por apenas  R$ 3.399,99",
-
-        "descricao" => "
-            Marca: Brastemp;
-            Modelo: BRE80AK;
-            Cor: Inox;
-            Tipo de painel de controle: Externo;
-            Capacidade: 394 L;
-            Capacidade do freezer: 179 L;
-            Peso: 108 kg;
-            Quantidade de níveis de temperatura: 5;
-            Eficiência energética: A;
-            Tipo de degelo: Frost free;
-        ",      
-
-        "garantia" => "Garantia: 12 meses." 
+        "valordesconto" => "Por apenas  R$ 3.399,99", 
     ],
     
     [        
@@ -35,22 +20,7 @@ $produtos = [
 
         "valor" => " De R$ 2.900,00",
 
-        "valordesconto" => "Por apenas R$ 2.299,99",
-
-        "descricao" => "
-            Marca: Brastemp;
-            Modelo: BWK16AB;
-            Cor: Prata;
-            Eficiência energética: A;
-            Capacidade: 16 kg;
-            Peso: 45 kg;
-            Quantidade de programas de lavagem: 12;
-            Níveis de água: 4;
-            Sistema de lavagem: Americano;
-            Tipos de carga de água: Manual, Automática;
-        ",
-
-        "garantia" => "Garantia: 16 meses." 
+        "valordesconto" => "Por apenas R$ 2.299,99", 
     ],
 
     [        
@@ -62,21 +32,6 @@ $produtos = [
         "valor" => "De R$ 1.000,00",
         
         "valordesconto" => "Por apenas R$ 799,99",
-
-        "descricao" => "
-        Modelo: ST66NBRU;
-        Cor: Preto;
-        Potência (W): 900W;
-        Capacidade: 34L;
-        Níveis de potência: 10;
-        Peso (kg): 14,8;
-        Altura (cm): 32,5;
-        Largura (cm): 52,0;
-        Comprimento (cm): 43,2;
-        SKU: 220V: 4968523 | 127V: 4968560;
-        ",
-
-        "garantia" => "Garantia: 8 meses." 
     ],
 
     [        
@@ -87,21 +42,6 @@ $produtos = [
         "valor" => "De R$ 2.200,00",
 
         "valordesconto" => "Por apenas R$ 1.799,99",
-
-        "descricao" => "
-            Marca: Semp TCL;
-            Modelo: 32RS530;
-            Voltagem: 110V/220V;
-            Tipo de tela: LED;
-            Tipo de resolução: HD;
-            Tamanho da tela(polegadas): 32;
-            Resolução máxima: 1366x768;
-            Taxa de atualização da tela: 60 Hz;
-            Peso: 3.8 kg;
-            Acessórios incluídos: Controle remoto;
-        ",    
-
-        "garantia" => "Garantias: 24 meses." 
     ],
 
     [        
@@ -112,21 +52,6 @@ $produtos = [
         "valor" => "De R$ 900,00",
 
         "valordesconto" => "Por apenas R$ 749,99",
-
-        "descricao" => "
-            Marca: Toyama;
-            Linha: Farmer;
-            Modelo: TCS53F-G2-18SN;
-            Cilindrada: 49.2 cc;
-            Tipos de combustível: Gasolina;
-            Potência: 2.81 hp
-            Peso: 5.32 kg
-            Velocidade máxima: 13500 rpm;
-            Capacidade do tanque: 0.55 L;
-            Acessórios incluídos: Kit ferramentas;
-        ",
-
-        "garantia" => "Garantia: 10 meses." 
     ],
 
     [      
@@ -137,21 +62,6 @@ $produtos = [
         "valor" => "De R$ 200,00",
 
         "valordesconto" => "Por apenas R$ 159,99",
-
-        "descricao" => "
-            Comprimento: 130cm;
-            Altura: 63cm;
-            Peças menores: 25x40 cm;
-            Peças médias: 25x51 cm;
-            Peça central: 25x63 cm;
-            Espaçamento entre as peças: 1 cm a 2 cm;
-            Espessura do produto: 2 CM;
-            Tecido: sintético;
-            Tela: Tecido com impressão digital ecológica;
-            Bordas: Com extensão da própria imagem nas laterais;
-        ",     
-
-        "garantia" => "Este produto não possue garantia" 
     ],
 
     [        
@@ -163,20 +73,6 @@ $produtos = [
 
         "valordesconto" => "Por apenas R$ 3.399,99",
 
-        "descricao" => "
-            Tipo: Windows;
-            Marca: Dell;
-            Modelo: I15-5566-A50B;
-            Polegadas: 15.6;
-            Sistema Operacional: Windows 10;
-            Processador: Intel Core i7;
-            Modelo: 7500U;
-            Núcleos: 2;
-            Velocidade: 2.7 GHz;
-            Cache: 4 MB;
-        ",
-
-        "garantia" => "18 meses." 
     ],
 
     [        
@@ -187,21 +83,6 @@ $produtos = [
         "valor" => "De R$190,00",
 
         "valordesconto" => "Por apenas R$ 159,99",
-
-        "descricao" => "
-            Marca: Wahl;
-            Departamento BS: Cabelos;
-            Indicado para: Dia a Dia;
-            Fonte de Energia: 110;
-            Formato: Com Fio;
-            Classificação: Caseira;
-            Peso do Produto: 0,47 kg;
-            Dimensões Aproximadas: 5 x 19 x 5 cm;
-            SAC: 0800 772 2560;
-            À prova de água: Não;
-        ",        
-
-        "garantia" => "22 meses." 
     ],
 
 ];
